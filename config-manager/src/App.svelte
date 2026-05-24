892bf561e59a73e7f6ddecff95e0fbd8692b63f6
minseok <71415819+minseokk7@users.noreply.github.com>
2026-05-25 03:31:07 +0900

    Bump version to 0.0.1.7

--- config-manager/src/App.svelte
+++ config-manager/src/App.svelte
@@ -77,7 +77,7 @@
     right_stick_min_y: -1,
     right_stick_max_y: 1
   };
-  const appVersion = '0.0.1.6';
+  const appVersion = '0.0.1.7';
   const releaseChannel = 'debug';
   const updateRepository = 'minseokk77/DS5Dongle';
   const updateStepOrder: UpdateStepCode[] = ['backup', 'checking', 'bootloader', 'copying', 'waiting', 'restoring', 'done'];
